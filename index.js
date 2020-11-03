c9e20d3fceae222e6e4c3a135f1860fd5b697cf2
Mariana Velásquez <mariana.velasquez0502@gmail.com>
2020-11-03 11:55:16 -0500

    estados online

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 const port = 3000;
+let loggedUsers=[];
 
 io.on('connection', (socket) => {
   console.log('a user connected');
@@ -10,6 +11,23 @@ io.on('connection', (socket) => {
   socket.on('newMsg', (msg) => {
     console.log(`Emitiendo nuevo mensaje: ${msg.content}`);
     io.emit('newMsg', msg);
+  });
+
+
+  socket.on('connected', (user) => {
+    console.log(`User conectado: ${user.telefono}`);
+    loggedUsers.push(user.telefono);
+    console.log("Connected",loggedUsers);
+    io.emit('connectedUsers', loggedUsers);
+    
+  });
+
+  socket.on('disconnected', (user) => {
+    console.log(`User desconectado: ${user.telefono}`);
+    let index=loggedUsers.indexOf(user.telefono);
+    loggedUsers.splice(index,1);
+    console.log("Disconeccted",loggedUsers);
+    io.emit('connectedUsers', loggedUsers);
   });
 
 });
